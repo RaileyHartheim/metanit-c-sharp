9cf597490442213e4c311c1191966eb233c1104b
raileyhartheim <raileyhartheim@outlook.com>
2021-11-05 18:27:56 +0500

    added operators overloading exercises

---
+++ operators_overloading/operators_overloading_sandwich/operators_overloading_sandwich/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace operators_overloading_sandwich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this should work after operators overload
+            Bread bread = new Bread { Weight = 80 };
+            Butter butter = new Butter { Weight = 20 };
+            Sandwich sandwich = bread + butter;
+            Console.WriteLine(sandwich.Weight); //100
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Bread
+    {
+        public int Weight { get; set; }
+
+        public static Sandwich operator +(Bread bread, Butter butter)
+        {
+            return new Sandwich { Weight = bread.Weight + butter.Weight };
+        }
+    }
+
+    class Butter
+    {
+        public int Weight { get; set; }
+
+    }
+
+    class Sandwich
+    {
+        public int Weight { get; set; }
+
+    }
+}
---
+++ operators_overloading/operators_overloading_states/operators_overloading_states/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace operators_overloading_states
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            State state1 = new State { Area = 170.3M, Population = 90000 };
+            State state2 = new State { Area = 47M, Population = 23000 };
+
+            State united_state = state1 + state2;
+            Console.WriteLine($"The area of united state is equal to {united_state.Area}. The population of this state is equal to {united_state.Population}.");
+
+            bool AreaIsLarger = state1 > state2;
+            bool AreaIsLess = state1 < state2;
+            Console.WriteLine($"The area of the first state is larger than the area of the second? {AreaIsLarger}\nThe area of the first state is less than the area of the second? {AreaIsLess}");
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class State
+    {
+        public decimal Population { get; set; }
+        public decimal Area { get; set; }
+
+        //addition operator overload
+        public static State operator +(State s1, State s2)
+        {
+            return new State { Area = s1.Area + s2.Area, Population = s1.Population + s2.Population };
+        }
+
+
+        //comparison operators overload (Area is chosen for comparing parameter)
+        public static bool operator >(State s1, State s2)
+        {
+            return s1.Area > s2.Area;
+        }
+        public static bool operator <(State s1, State s2)
+        {
+            return s1.Area < s2.Area;
+        }
+
+    }
+}
