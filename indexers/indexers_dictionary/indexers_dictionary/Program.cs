b86ca4cf97d29f5cd956ac4b1e067445c00aaf6a
raileyhartheim <raileyhartheim@outlook.com>
2021-11-06 12:22:38 +0500

    add indexers exercises

---
+++ indexers/indexers_dictionary/indexers_dictionary/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace indexers_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary dict = new Dictionary();
+            Console.WriteLine(dict["blue"]);
+            dict["blue"] = "голубой";
+            Console.WriteLine(dict["blue"]);
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Word
+    {
+        public string Source { get; }
+        public string Target { get; set; }
+
+        public Word(string source, string target)
+        {
+            Source = source;
+            Target = target;
+        }
+    }
+
+    class Dictionary
+    {
+        Word[] words;
+
+        public Dictionary()
+        {
+            words = new Word[]
+            {
+                new Word("red", "красный"),
+                new Word("blue", "синий"),
+                new Word("green", "зеленый")
+            };
+        }
+
+        public string this[string source]
+        {
+            get
+            {
+                Word word = null;
+                foreach (Word w in words)
+                {
+                    if (w.Source == source)
+                    {
+                        word = w;
+                        break;
+                    }
+                }
+                return word?.Target;
+            }
+            set
+            {
+                foreach (var w in words)
+                {
+                    if (w?.Source == source)
+                    {
+                        w.Target = value;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ indexers/indexers_football_team/indexers_football_team/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+//TODO: to think about IndexOutofRangeException
+
+namespace indexers_football_team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Team manchester = new Team();
+            manchester[0] = new Player { FullName = "David de Gea", Number = 1 };
+            manchester[1] = new Player { FullName = "Victor Lindelof", Number = 2 };
+            Console.WriteLine(manchester[0].FullName);
+            Console.WriteLine(manchester[1].Number);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Player
+    {
+        public string FullName { get; set; }
+        public int Number { get; set; }
+    }
+
+    class Team
+    {
+        Player[] players;
+
+        public Team()
+        {
+            players = new Player[11];
+        }
+
+        public Player this[int index]
+        {
+            get
+            {
+                return players[index];
+            }
+            set
+            {
+                players[index] = value;
+            }
+        }
+    }
+}
